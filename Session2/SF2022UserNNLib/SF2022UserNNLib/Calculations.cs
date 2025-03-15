cddc77e816f08a008caba809c7aa5e246ddf2172
Otter <130929560+OtterUnderwater@users.noreply.github.com>
2025-03-15 06:30:17 +0300

    test: add unit-tests

---
+++ Session2/SF2022UserNNLib/SF2022UserNNLib/Calculations.cs
@@ -0,0 +1,79 @@
+﻿namespace SF2022UserNNLib
+{
+    public class Calculations
+    {
+
+		/// <summary>
+		/// Возвращает список свободных временных интервалов.
+		/// </summary>
+		/// <param name="startTimes">Массив начал занятых промежутков</param>
+		/// <param name="durations">Массив длительностей занятых промежутков</param>
+		/// <param name="beginWorkingTime">Начало рабочего дня</param>
+		/// <param name="endWorkingTime">Конец рабочего дня</param>
+		/// <param name="consultationTime">Минимальное необходимое время для работы</param>
+		/// <returns>Список свободных интервалов в формате HH:mm-HH:mm</returns>
+		public string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime,
+			TimeSpan endWorkingTime, int consultationTime)
+		{
+			List<string> freeIntervals = new List<string>();
+
+			if (consultationTime <= 0)
+			{
+				freeIntervals.Add("Минимальное время должно быть больше нуля.");
+				return freeIntervals.ToArray();
+			}
+			if (beginWorkingTime == new TimeSpan(0, 0, 0) || endWorkingTime == new TimeSpan(0, 0, 0))
+			{
+				freeIntervals.Add("Начало и конец рабочего дня должны быть указаны.");
+				return freeIntervals.ToArray();
+			}
+			if (beginWorkingTime >= endWorkingTime)
+			{
+				freeIntervals.Add("Начало рабочего дня должно быть раньше его окончания.");
+				return freeIntervals.ToArray();
+			}
+			if (startTimes.Length != durations.Length)
+			{
+				freeIntervals.Add("Массивы startTimes и durations должны быть одинаковой длины.");
+				return freeIntervals.ToArray();
+			}
+			TimeSpan consultation = new TimeSpan(0, consultationTime, 0);
+
+			// Список занятых интервалов
+			var busyIntervals = new List<(TimeSpan Start, TimeSpan End)>();
+			for (int i = 0; i < startTimes.Length; i++)
+			{
+				TimeSpan start = startTimes[i];
+				TimeSpan end = start + new TimeSpan(0, durations[i], 0);
+				busyIntervals.Add((start, end));
+			}
+			busyIntervals = busyIntervals.OrderBy(a => a.Start).ToList();
+			TimeSpan currentStart = beginWorkingTime;
+			foreach (var bi in busyIntervals)
+			{
+				if (bi.Start > currentStart)
+				{
+					var freeEnd = bi.Start;
+					while (currentStart + consultation <= freeEnd)
+					{
+						var intervalEnd = currentStart + consultation;
+						freeIntervals.Add($"{currentStart:hh\\:mm}-{intervalEnd:hh\\:mm}");
+						currentStart = intervalEnd;
+					}
+
+				}
+				currentStart = bi.End > currentStart ? bi.End : currentStart;
+			}
+			if (endWorkingTime - currentStart >= consultation)
+			{
+				while (currentStart + consultation <= endWorkingTime)
+				{
+					var intervalEnd = currentStart + consultation;
+					freeIntervals.Add($"{currentStart:hh\\:mm}-{intervalEnd:hh\\:mm}");
+					currentStart = intervalEnd;
+				}
+			}
+			return freeIntervals.ToArray();
+		}
+	}
+}
---
+++ Session2/SF2022UserNNLib/UnitTestsCalculations/UnitTests.cs
@@ -0,0 +1,182 @@
+﻿using SF2022UserNNLib;
+
+namespace UnitTestsCalculations
+{
+	[TestClass]
+	public sealed class UnitTests
+	{
+		private Calculations calc = new Calculations();
+
+		/// <summary>
+		///  Нет занятых интервалов, день свободен
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_NoBusyIntervals_ReturnFullDay()
+		{
+			var startTimes = new TimeSpan[] { };
+			var durations = new int[] { };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 60;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "09:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00", "16:00-17:00" }, result);
+		}
+
+		/// <summary>
+		/// Один занятый интервал в начале дня
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_BusyIntervalAtStart_ReturnFreeIntervals()
+		{
+			var startTimes = new[] { new TimeSpan(9, 0, 0) };
+			var durations = new[] { 60 };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 60;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00", "16:00-17:00" }, result);
+		}
+
+		/// <summary>
+		/// Один занятый интервал в середине дня
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_OneBusyInterval_ReturnFreeIntervals()
+		{
+			var startTimes = new[] { new TimeSpan(10, 0, 0) };
+			var durations = new[] { 60 };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 60;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "09:00-10:00", "11:00-12:00", "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00", "16:00-17:00" }, result);
+		}
+
+		/// <summary>
+		/// Один занятый интервал в конце дня
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_BusyIntervalAtEnd_ReturnFreeIntervals()
+		{
+			var startTimes = new[] { new TimeSpan(16, 0, 0) };
+			var durations = new[] { 60 };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 60;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "09:00-10:00", "10:00-11:00", "11:00-12:00", "12:00-13:00", "13:00-14:00", "14:00-15:00", "15:00-16:00" }, result);
+		}
+
+		/// <summary>
+		/// Несколько занятых интервалов
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_OverlappingBusyIntervals_ReturnFreeIntervals()
+		{
+			var startTimes = new[] { new TimeSpan(10, 0, 0), new TimeSpan(10, 30, 0) };
+			var durations = new[] { 60, 30 };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 30;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(
+				new[] { "09:00-09:30", "09:30-10:00", "11:00-11:30", "11:30-12:00", "12:00-12:30", "12:30-13:00", "13:00-13:30",
+					"13:30-14:00", "14:00-14:30", "14:30-15:00", "15:00-15:30", "15:30-16:00", "16:00-16:30", "16:30-17:00" }, result);
+		}
+
+		/// <summary>
+		/// Начало рабочего дня позже конца
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_BeginWorkingTimeAfterEnd_ReturnError()
+		{
+			var startTimes = new TimeSpan[] { };
+			var durations = new int[] { };
+			var beginWorkingTime = new TimeSpan(17, 0, 0);
+			var endWorkingTime = new TimeSpan(9, 0, 0);
+			var consultationTime = 30;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "Начало рабочего дня должно быть раньше его окончания." }, result);
+		}
+
+		/// <summary>
+		/// Начало или конец рабочего дня не указаны
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_NullBeginTimeOrEndTime_ReturnError()
+		{
+			var startTimes = new TimeSpan[] { };
+			var durations = new int[] { };
+			var beginWorkingTime = new TimeSpan(0, 0, 0);
+			var endWorkingTime = new TimeSpan(0, 0, 0);
+			var consultationTime = 30;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "Начало и конец рабочего дня должны быть указаны." }, result);
+		}
+
+		/// <summary>
+		/// Минимальное время меньше или равно нулю
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_IncorrectConsultationTime_ReturnError()
+		{
+			var startTimes = new TimeSpan[] { };
+			var durations = new int[] { };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = -10;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "Минимальное время должно быть больше нуля." }, result);
+		}
+
+		/// <summary>
+		/// Массивы startTimes и durations разной длины
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_ArraysLengthDifferent_ReturnError()
+		{
+			var startTimes = new[] { new TimeSpan(10, 0, 0) };
+			var durations = new int[] { };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 30;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new[] { "Массивы startTimes и durations должны быть одинаковой длины." }, result);
+		}
+
+		/// <summary>
+		/// Занятые интервалы покрывают весь день
+		/// </summary>
+		[TestMethod]
+		public void AvailablePeriods_FullDayBusy_ReturnEmptyArray()
+		{
+			var startTimes = new[] { new TimeSpan(9, 0, 0) };
+			var durations = new[] { 480 };
+			var beginWorkingTime = new TimeSpan(9, 0, 0);
+			var endWorkingTime = new TimeSpan(17, 0, 0);
+			var consultationTime = 30;
+
+			var result = calc.AvailablePeriods(startTimes, durations, beginWorkingTime, endWorkingTime, consultationTime);
+
+			CollectionAssert.AreEqual(new string[] { }, result);
+		}
+	}
+}
